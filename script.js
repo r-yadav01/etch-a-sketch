68026fa73c1a1ab1a9a4256767dd25061d25ccf4
Rohan Yadav <rohan151025@gmail.com>
2024-09-09 11:50:58 +0530

    Create container div to include all other divs

--- script.js
+++ script.js
@@ -4,17 +4,30 @@ row.classList.add('row');
 let pageHeight = window.innerHeight;
 let pageWidth = window.innerHeight;
 
+const body = document.querySelector('body');
+const container = document.createElement('div');
+container.classList.add('container');
+body.appendChild(container);
+
+
 // get the length of each side of the row elements
 let divSide;
-if (pageHeight >= pageWidth)
-    divSide = pageWidth/16;
-else if (pageHeight < pageWidth)
-    divSide = pageHeight/16;
+if (pageHeight >= pageWidth) {
+    container.style.width = pageWidth - 20 + 'px';
+    container.style.height = container.style.width;
+    divSide = (pageWidth-20)/16;
+}
+else if (pageHeight < pageWidth) {
+    container.style.width = pageHeight - 20 + 'px';
+    container.style.height = container.style.width;
+    divSide = (pageWidth-20)/16;
+}
+
 
 // create a single div inside a row
 let divItem = document.createElement('div');
 divItem.classList.add('row_item');
 divItem.style.width = `${divSide}px`;
 divItem.style.height = `${divSide}px`;
-const body = document.querySelector('body');
-body.appendChild(divItem);
+container.appendChild(divItem);
+

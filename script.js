d3716d55543094863336282eabf7e54e0e8a4559
Rohan Yadav <rohan151025@gmail.com>
2024-09-09 15:36:48 +0530

    Create the container for the squares
    well I am stupid enough to again jump directly on the keyboard without
    understanding the whole problem to solve, so as expected it took much
    more time than expected. After understanding the problem, writing the
    steps to solve the problem on a paper, it took less than 30 mins to
    create the container..

--- script.js
+++ script.js
@@ -1,33 +1,41 @@
-let row = document.createElement('div');
-row.classList.add('row');
+function createContainer (numSquare = 16) {
+    const container = document.createElement('div');
+    container.classList.add('container');
 
-let pageHeight = window.innerHeight;
-let pageWidth = window.innerHeight;
+    let pageHeight = window.innerHeight;
+    let pageWidth = window.innerWidth;
 
-const body = document.querySelector('body');
-const container = document.createElement('div');
-container.classList.add('container');
-body.appendChild(container);
-
-
-// get the length of each side of the row elements
-let divSide;
-if (pageHeight >= pageWidth) {
-    container.style.width = pageWidth - 20 + 'px';
+    let contSide;
+    if (pageHeight >= pageWidth) {
+        container.style.width = (pageWidth - 20) + 'px';
+        contSide = pageWidth - 20;
+    }
+    else if (pageHeight < pageWidth) {
+        container.style.width = (pageHeight - 20) + 'px';
+        contSide = pageHeight - 20;
+    }
     container.style.height = container.style.width;
-    divSide = (pageWidth-20)/16;
-}
-else if (pageHeight < pageWidth) {
-    container.style.width = pageHeight - 20 + 'px';
-    container.style.height = container.style.width;
-    divSide = (pageWidth-20)/16;
-}
 
+    let squareSide = contSide/numSquare;
 
-// create a single div inside a row
-let divItem = document.createElement('div');
-divItem.classList.add('row_item');
-divItem.style.width = `${divSide}px`;
-divItem.style.height = `${divSide}px`;
-container.appendChild(divItem);
+    for (let i = 0; i < numSquare; i++) {
+        let row = document.createElement('div');
+        row.classList.add('row');
 
+        for (let i = 0; i < numSquare; i++) {
+            let square = document.createElement('div');
+            square.classList.add('row_square');
+            square.style.width = squareSide + 'px';
+            square.style.height = squareSide + 'px';
+            row.appendChild(square);
+        }
+        container.appendChild(row);
+    }
+
+    const body = document.querySelector('body');
+    body.appendChild(container);
+} 
+
+document.addEventListener('DOMContentLoaded', () => {
+    createContainer();
+})
